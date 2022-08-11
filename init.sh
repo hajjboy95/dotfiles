f2e8cc20e7edf761f7362d91c4a23e407bf4c75a
hajjboy95 <gizmo-boy95@hotmail.com>
2022-08-11 11:46:05 +0100

    updates init to check for source

--- init.sh
+++ init.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/zsh
 
 cp .my* ~/
 
 for file in ~/.my-*; do
     [ -e "$file" ] || continue
-    echo "source $file" >> ~/.zshrc
+
+    if grep -R "$file" ~/.zshrc
+    then
+        echo "exists already $file"
+        # do nothing since it exists
+    else
+        echo "source $file" >> ~/.zshrc
+    fi
 done
+
+source ~/.zshrc
 
